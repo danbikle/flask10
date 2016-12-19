946a178b219dfd6b14222a04d7cf84b8f7cfc803
Dan Bikle <bikle@bikle.com>
2016-12-19 14:37:26 -0800

    more

--- flask10.py
+++ flask10.py
@@ -3,9 +3,11 @@
 # http://flask.pocoo.org/
 
 # Demo1:
+# export FLASK_DEBUG=1
 # export FLASK_APP=flask10.py
 # flask run
 # curl 127.0.0.1:5000/
+# Above syntax offers 'auto-reload' feature.
 
 # Demo2:
 # python flask10.py
@@ -13,7 +15,8 @@
 # Demo3:
 # gunicorn wsgi
 
-# import pdb
+# Demo2, Demo3 offer one-shell-command simplicity.
+
 import os
 from   flask import Flask
 application = Flask(__name__)
